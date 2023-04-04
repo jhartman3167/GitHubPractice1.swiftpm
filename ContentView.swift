32a7b6b602a06dfa735a7bfaa46bc2ddf513088c
Michael Grabowy <mgrabowy3150@stu.d214.org>
2023-04-04 14:25:12 -0500

    Commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Multiply") {
+            <#code#>
         }
+        Text("\(answer)")
+    
     }
 }
